af944cc66d8130add9336422907f84fcc8049ced
Kushal Hada <kushaldeveloper@gmail.com>
2020-12-08 12:36:11 -0500

    update text in controller

--- MyNetFrameworkFourEight.Tests/Controllers/HomeControllerTest.cs
+++ MyNetFrameworkFourEight.Tests/Controllers/HomeControllerTest.cs
@@ -35,7 +35,7 @@ namespace MyNetFrameworkFourEight.Tests.Controllers
             ViewResult result = controller.About() as ViewResult;
 
             // Assert
-            Assert.AreEqual("Your application description page.", result.ViewBag.Message);
+            Assert.AreEqual("Welcome to legacy .NET Framework!", result.ViewBag.Message);
         }
 
         [TestMethod]
--- MyNetFrameworkFourEight/Controllers/HomeController.cs
+++ MyNetFrameworkFourEight/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace MyNetFrameworkFourEight.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Welcome to legacy .NET Framework!";
 
             return View();
         }
